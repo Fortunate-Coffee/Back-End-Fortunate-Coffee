a1b2a039564263d1c39cc2813d7131d5b720c1b7
fndsjy <fendysanjaya40@gmail.com>
2024-06-21 04:32:33 +0700

    Deploying to Railway

--- config/database.js
+++ config/database.js
@@ -1,4 +1,3 @@
-const pg = require('pg');
 const {
   PGHOST = "localhost",
   PGPORT = "5432",
@@ -6,38 +5,21 @@ const {
   PGPASSWORD = "fendys",
   PGDATABASE = "fortunate-coffee",
 
-  // RPGHOST = "viaduct.proxy.rlwy.net",
-  // RPGPORT = "54940",
-  // RPGUSER = "postgres",
-  // RPGPASSWORD = "HhYtTjHIxVqDQsGbLAwmkLJXQDnTKdDA",
-  // RPGDATABASE = "railway",
-
-  POSTGRES_URL="postgres://default:2xRW5XwjBuSo@ep-blue-firefly-a1qiuusg-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require",
-  POSTGRES_PRISMA_URL="postgres://default:2xRW5XwjBuSo@ep-blue-firefly-a1qiuusg-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require&pgbouncer=true&connect_timeout=15",
-  POSTGRES_URL_NO_SSL="postgres://default:2xRW5XwjBuSo@ep-blue-firefly-a1qiuusg-pooler.ap-southeast-1.aws.neon.tech:5432/verceldb",
-  POSTGRES_URL_NON_POOLING="postgres://default:2xRW5XwjBuSo@ep-blue-firefly-a1qiuusg.ap-southeast-1.aws.neon.tech:5432/verceldb?sslmode=require",
-  POSTGRES_USER="default",
-  POSTGRES_HOST="ep-blue-firefly-a1qiuusg-pooler.ap-southeast-1.aws.neon.tech",
-  POSTGRES_PASSWORD="2xRW5XwjBuSo",
-  POSTGRES_DATABASE="verceldb",
-
+  RPGHOST = "viaduct.proxy.rlwy.net",
+  RPGPORT = "55396",
+  RPGUSER = "postgres",
+  RPGPASSWORD = "qwmgtIBSkTFYGUvQHwVoSZgfJBOIARoH",
+  RPGDATABASE = "railway",
 } = process.env;
 
 module.exports = {
   development: {
-    username: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    database: POSTGRES_DATABASE,
-    host: POSTGRES_HOST,
-    dialect: 'postgres',
-    dialectModule: pg,
+    username: PGUSER,
+    password: PGPASSWORD,
+    database: PGDATABASE,
+    host: PGHOST,
     port: PGPORT,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    dialect: "postgres"
   },
   test: {
     username: PGUSER,
@@ -48,18 +30,11 @@ module.exports = {
     dialect: "postgres"
   },
   production: {
-    username: POSTGRES_USER,
-    password: POSTGRES_PASSWORD,
-    database: POSTGRES_DATABASE,
-    host: POSTGRES_HOST,
-    dialect: 'postgres',
-    dialectModule: pg,
-    port: PGPORT,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false,
-      },
-    },
+    username: RPGUSER,
+    password: RPGPASSWORD,
+    database: RPGDATABASE,
+    host: RPGHOST,
+    port: RPGPORT,
+    dialect: "postgres"
   }
 }
